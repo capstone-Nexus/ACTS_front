d7f95882b15c696a497cd9e363bccce06b29a813
yihyle <hslee050320@naver.com>
2025-10-31 22:29:55 +0900

    로딩 컴포넌트 개발

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,9 +1,11 @@
-import React from "react"
+import Loadinggif from "@/public/images/loading.gif";
+import Image from "next/image";
 
 export default function Loading() {
   return (
-    <>
-
-    </>
+    <div className="w-full h-screen bg-[#F9FAFB] center flex-col gap-5">
+      <Image src={Loadinggif} alt="loading" className="w-[100px] h-[100px]"/>
+      <p className="text-[32px] font-medium text-[#4A8AEE]">Loading</p>
+    </div>
   )
 }
